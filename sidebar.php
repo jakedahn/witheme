ed2666440d9ab9f3742c415c6267522d6230c17d
Jake Dahn <admin@jakedahn.com>
2008-07-27 02:28:20 -0500

    first upping of files

--- sidebar.php
+++ sidebar.php
@@ -12,6 +12,6 @@
     <div id="sidebar">
       <h3>Outside the Box</h3>
       <ul>
-      <?php c2c_get_recent_posts($num_posts=3,  $format = "<li><span class=\"linkauthor\">%post_author%</span> <br /> <div class=\"linkdesc\">%post_content%</div></li>", $categories = '9'); ?>
+      <?php c2c_get_recent_posts($num_posts=3,  $format = "<li><span class=\"linkauthor\">%post_author%</span> <br /> <div class=\"linkdesc\"><img src=\"http://looce.com/wordpress/wp-content/themes/wakingideas/images/icon_track.png\" alt=\"link\" /> %post_content%</div></li>", $categories = '9'); ?>
       </ul>
     </div>
