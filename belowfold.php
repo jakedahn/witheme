fbd53475930b12badb5f21a2c95f96968f8f71b2
Jake Dahn <admin@jakedahn.com>
2008-07-27 22:00:19 -0500

    updating

--- belowfold.php
+++ belowfold.php
@@ -6,21 +6,21 @@
   <div class="column" id="firstcol">
     <h3 id="culture">Culture</h3>
     <ul id="culture">
-      <?php c2c_get_recent_posts($num_posts=5,  $format = "<li><span class=\"highlightlink\"><img src=\"http://looce.com/wordpress/wp-content/themes/wakingideas/images/icon_track.png\" alt=\"link\" />  %post_URL%</span><br /> %post_excerpt_short%</li>", $categories = '4'); ?>
+      <?php c2c_get_recent_posts($num_posts=5,  $format = "<li><span class=\"highlightlink\"><img src=\"http://looce.com/wordpress/wp-content/themes/wakingideas/images/icon_track.png\" alt=\"link\" />  %post_URL%</span><br /> %post_excerpt_short%</li>", $categories = '11'); ?>
     </ul>
   </div>
   
   <div class="column">
     <h3 id="politics">Politics</h3>
     <ul id="politics">
-       <?php c2c_get_recent_posts($num_posts=5,  $format = "<li><span class=\"highlightlink\"><img src=\"http://looce.com/wordpress/wp-content/themes/wakingideas/images/icon_track.png\" alt=\"link\" />  %post_URL%</span><br /> %post_excerpt_short%</li>", $categories = '5'); ?>
+       <?php c2c_get_recent_posts($num_posts=5,  $format = "<li><span class=\"highlightlink\"><img src=\"http://looce.com/wordpress/wp-content/themes/wakingideas/images/icon_track.png\" alt=\"link\" />  %post_URL%</span><br /> %post_excerpt_short%</li>", $categories = '10'); ?>
     </ul>
   </div>
   
   <div class="column">
     <h3 id="music">Music</h3>
     <ul id="music">
-       <?php c2c_get_recent_posts($num_posts=5,  $format = "<li><span class=\"highlightlink\"><img src=\"http://looce.com/wordpress/wp-content/themes/wakingideas/images/icon_track.png\" alt=\"link\" />  %post_URL%</span><br /> %post_excerpt_short%</li>", $categories = '11'); ?>
+       <?php c2c_get_recent_posts($num_posts=5,  $format = "<li><span class=\"highlightlink\"><img src=\"http://looce.com/wordpress/wp-content/themes/wakingideas/images/icon_track.png\" alt=\"link\" />  %post_URL%</span><br /> %post_excerpt_short%</li>", $categories = '9'); ?>
     </ul>
   </div>
   
