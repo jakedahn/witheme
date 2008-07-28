baf8e0938ffb7b31efefcc45aee5dd9e0b3f49ea
Jake Dahn <admin@jakedahn.com>
2008-07-27 22:42:18 -0500

    updating

--- footer.php
+++ footer.php
@@ -2,8 +2,6 @@
 		<p>
 			&copy; <?php echo(date('Y')); ?> Waking Ideas
 			|
-			A <a href="http://wordpress.org/" title="WordPress">WordPress</a> Blog
-			|
 			Valid <a href="http://validator.w3.org/check?uri=<?php echo get_settings('home'); ?>&amp;outline=1&amp;verbose=1" title="Valid XHTML 1.0 Strict" rel="nofollow">XHTML</a> &amp; <a href="http://jigsaw.w3.org/css-validator/validator?uri=<?php bloginfo('stylesheet_url'); ?>&amp;profile=css2&amp;warning=no" title="Valid CSS" rel="nofollow">CSS</a>
 			<?php do_action('wp_footer'); ?>
 		</p>
