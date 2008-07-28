97ad10c73fba3c34a6f5faa65b1b227e113535e4
Jake Dahn <admin@jakedahn.com>
2008-07-27 21:14:48 -0500

    updating

--- sidebar.php
+++ sidebar.php
@@ -12,13 +12,13 @@
       </ul>
       <h3>Issue Highlights</h3>
       <ul id="highlights">
-        <?php c2c_get_recent_posts($num_posts=7,  $format = "<li><span class=\"highlightlink\"><img src=\"http://looce.com/wordpress/wp-content/themes/wakingideas/images/icon_track.png\" alt=\"link\" />  %post_URL%</span><br /> %post_excerpt%</div></li>"); ?>
+        <?php c2c_get_recent_posts($num_posts=5,  $format = "<li><span class=\"linkauthor\">%post_author%</span> <br /> <div class=\"linkdesc\"><img src=\"http://wakingideas.com/wordpress/wp-content/themes/wakingideas/images/icon_track.png\" alt=\"link\" /> %post_excerpt%</div></li>"); ?>
       </ul>    
     </div>
 
     <div id="sidebar"> 
       <h3>Outside the Box</h3>
       <ul>
-      <?php c2c_get_recent_posts($num_posts=7,  $format = "<li><span class=\"linkauthor\">%post_author%</span> <br /> <div class=\"linkdesc\"><img src=\"http://looce.com/wordpress/wp-content/themes/wakingideas/images/icon_track.png\" alt=\"link\" /> %post_content%</div></li>", $categories = '7'); ?>
+      <?php c2c_get_recent_posts($num_posts=7,  $format = "<li><span class=\"linkauthor\">%post_author%</span> <br /> <div class=\"linkdesc\"><img src=\"http://wakingideas.com/wordpress/wp-content/themes/wakingideas/images/icon_track.png\" alt=\"link\" /> %post_content%</div></li>", $categories = '7'); ?>
       </ul>
     </div>
